625067d71439537c79ea7c4bfc55c8f24c9edb24
Aaron Paul Greyling <aaron.p.greyling@gmail.com>
2024-05-24 11:05:40 +0200

    Updated Bento Grid

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -1,18 +1,34 @@
-import { gridItemns } from '@/data';
+import { gridItems } from '@/data';
 import { BentoGrid, BentoGridItem } from './ui/BentoGrid';
 
 const Grid = () => {
   return (
     <section id="about">
       <BentoGrid>
-        {gridItemns.map((item) => (
-          <BentoGridItem
-            id={item.id}
-            key={item.id}
-            title={item.title}
-            description={item.description}
-          />
-        ))}
+        {gridItems.map(
+          ({
+            id,
+            title,
+            description,
+            className,
+            img,
+            imgClassName,
+            titleClassName,
+            spareImg,
+          }) => (
+            <BentoGridItem
+              id={id}
+              key={id}
+              title={title}
+              description={description}
+              className={className}
+              img={img}
+              imgClassName={imgClassName}
+              titleClassName={titleClassName}
+              spareImg={spareImg}
+            />
+          )
+        )}
       </BentoGrid>
     </section>
   );
